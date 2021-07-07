6e160de0518bcee3c5960022f92b3ce490253267
MICHAEL BAZAN SANCHEZ <mbazansanchez@cnm.edu>
2021-07-07 15:19:41 -0600

    Add project files.

---
+++ Quiz1/Class.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Quiz1
+{
+    class Gforce
+    {
+        
+        double Mass1;
+        double Mass2;
+        double Distance;
+        double GraviForce;
+
+       public void SetMass1(double value)
+        {
+            this.Mass1 = value;
+        }
+
+
+        public double GetMass1()
+        {
+            return Mass1;
+        }
+
+        public void SetMass2(double value)
+        {
+            this.Mass2 = value;
+
+        }
+
+        public double GetMass2()
+        {
+            return Mass1;
+        }
+
+        public void SetDistance(double value)
+        {
+            this.Distance = value;
+        }
+
+        public double GetDistance()
+        {
+            
+            return Distance;
+        }
+
+
+        private void setGraviForce()
+        {
+            
+            double G = 6.67408 * Math.Pow(10, -11);
+            GraviForce = G * Mass1 * Mass2 / Math.Pow(Distance, 2); 
+        }
+
+        public double GetGraviForce()
+        {
+            setGraviForce();
+            return GraviForce;
+        }
+    }
+}
---
+++ Quiz1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Quiz1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string doAgain = "yes";
+            string smass1;
+            string smass2;
+            string sDistance;
+            double mass1;
+            double mass2;
+            double Distance;
+            double Gforces;
+            
+            do
+            {
+
+                Gforce force = new Gforce();
+
+                Console.Out.WriteLine("Please enter in the Mass of object 1 in kg");
+                smass1 = Console.ReadLine();
+                mass1 = double.Parse(smass1);
+                force.SetMass1(mass1);
+
+                Console.Out.WriteLine("Please enter in the Mass of object 2 in kg");
+                smass2 = Console.ReadLine();
+                mass2 = double.Parse(smass2);
+                force.SetMass2(mass2);
+
+    
+                force.SetMass2(mass2);
+                Console.Out.WriteLine("Please enter in the Distance between the 2 Objects in meters;");
+                sDistance = Console.ReadLine();
+                Distance = double.Parse(sDistance);
+                force.SetDistance(Distance);
+
+                Gforces = force.GetGraviForce();
+                Console.Out.WriteLine("The Gravitational force between these 2 ojects is: " + Gforces);
+
+                Console.Out.WriteLine("Would you like to do another? yes/no: ");
+                doAgain = Console.ReadLine().ToLower();
+
+
+
+            } while (doAgain == "yes");
+
+            Console.Out.WriteLine("GoodBye!");
+        }
+    }
+}
